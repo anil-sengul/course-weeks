f01d41361ebf286cad56ae0f50771d04c2422e96
anil-sengul <69522098+anil-sengul@users.noreply.github.com>
2020-09-22 15:55:57 +0200

    One hor coding session - index.html andstyle.css files are updated.

---
+++ week03/tuesday/exercises/exercise1.js
@@ -0,0 +1,13 @@
+
+/* 
+    1- Get an input from user.
+        - Only digits.
+    2- Create an digit array with every individual digit.
+    3- Sort the digits in numerical order.
+    4- print all the numbers.
+
+    Get input from user only with digits
+    Create an array, and assign every individual digits of the input  to this array.
+    Start to from seconarray items with its negihbour:
+        If it is smaller than the its neighbor
+ */
