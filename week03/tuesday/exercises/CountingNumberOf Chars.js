db5f20a19a7c90ead017ddc7ecce03b5d54b0b7b
anil-sengul <69522098+anil-sengul@users.noreply.github.com>
2020-09-25 16:38:17 +0200

    CountinfNumberOfChars.js exercises is added.

---
+++ week03/tuesday/exercises/CountingNumberOf Chars.js
@@ -0,0 +1,63 @@
+/*
+List inputs, process and outputs
+Write test cases
+Find constraints
+Write the algorithm in pseudocode
+*/
+
+/* 
+2. Counting the number of characters (60 min)
+Create a program that prompts for an input string and 
+displays output that shows the input string and the 
+number of characters the string contains.
+
+inputs  : String Input 
+process : Prompt String Input, Count the number of charechters in the stringInput
+outputs : String Input, and Number of Chars
+
+Test Cases
+==========
+input           : Abcde
+Expected Result : Abcde 5
+Actual Result   : Abcde 5
+
+input           : 
+Expected Result :  , 0
+Actual Result   :  , 0
+
+
+Constraints
+===========
+- Letters are accepted for the Stirng Input.
+- Numbers are accepted for the String Input.
+- Special chars are accepted for the String Input.
+
+CountingNumberOfChars
+    INITIALIZE stringInput as ""
+    INITIALIZE counter as 0
+
+    STEP 1:
+    PROMPT stringInput with "What is your name?"
+    
+    STEP 2:
+    counter = length of the stringInput
+
+    STEP 3:
+    DISPLAY stringInput + " " + counter
+
+END CountingNumberOfChars
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
